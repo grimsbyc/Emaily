c3e1dad6b02475bca3871b1530f07747294ac962
grimsbyc <cschouten1@gmail.com>
2023-12-28 11:27:07 -0500

    new test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI, { ssl: true });
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
